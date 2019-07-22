5df996c60f26f52ef2a6ae16346cd40b28ae2518
Raiu1210 <baio1484@gmail.com>
2019-07-22 22:36:05 +0900

    added function for uploading .png image

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const multer = require('multer');
 const bodyParser = require('body-parser')
 const bitcoinMessage = require('bitcoinjs-message')
 const cors = require('cors')
-const app = express()
 
+
+const app = express()
 const messagePrefix = "\x19Monacoin Signed Message:\n"
 
 var fs = require('fs');
@@ -18,6 +20,8 @@ var options = {
 
 var server = https.createServer(options,app);
 
+
+// about digital signature by mpurse
 app.post('/verify', function(req, res) {
   var date = new Date();
   var a = date.getTime();
@@ -32,5 +36,43 @@ app.post('/verify', function(req, res) {
     message: is_varify
   })
 })
+
+
+
+// about uploading goods_info
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, './img')
+  },
+  
+  filename: function (req, file, cb) {
+      //　Math.random().toString(36).slice(-9)で乱数を生成
+      const imageName = `${Math.random().toString(36).slice(-9)}_${Date.now()}.png`
+      cb(null, imageName)
+  }
+})
+
+const upload = multer({
+  storage: storage
+}).single('file')
+
+app.post('/image', (req, res) => {
+  upload(req, res, (err) => {
+      if (err) {
+          //アップロード失敗した場合
+          res.json({
+              status: "error",
+              error: "fail to uplord image"
+          })
+      } else {
+          //アップロード成功した場合
+          res.json({
+              status: "sucess",
+              // ファイル名を返す
+              path: res.req.file.filename
+          })
+      }
+  })
+});
 
 server.listen(3000);
