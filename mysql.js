7d40098a521853ad56a1d94ec1f053cafbe54989
Raiu1210 <baio1484@gmail.com>
2019-07-24 02:22:08 +0900

    mysql

---
+++ mysql.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'raiu',
+    password : '',
+    database: 'mona_marche'
+  });
+
+  connection.connect();
+
+  let sql = 'show tables;';
+
+  connection.query(sql, (err, rows, fields) => {
+    if (err) throw err;
+  
+    console.log(rows);
+  });
+  
+  connection.end();
